144cfc566d50ac786bca4973abdd6252d52eb6b7
kaywish <kaywishawaan@gmail.com>
2022-11-10 16:28:05 -0500

    added some css

--- src/components/CalForm.js
+++ src/components/CalForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./calform.css" 
 
 function CalForm() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import "./header.css"
 function Header({totalCal}) {
 
   return (
     <header>
-        <h1> Calorie Tracker  </h1>
+        <h1 className="cal-trac"> Calorie Tracker  </h1>
         <div className="total-cal"> Total Calories: {totalCal} 
         </div>
 
