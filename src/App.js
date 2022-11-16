ccd08f39b24bb83ffbc239914e80c9cb985c7e7c
kaywish <kaywishawaan@gmail.com>
2022-11-16 09:22:53 -0500

    data displays after fetch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Header from "./components/Header"
 import CalForm from "./components/CalForm"
 function App() {
 
   const [calories, setCal] = useState([])
   const [totalCal, setTotalCal] = useState(0)
+  const [food, setFood] = useState([])
 
+let baseUrl= "http://localhost:3001"
+
+// Fetching from the backend
+const foods = () => {
+  fetch(baseUrl + "/food")
+  .then(res => {
+    return res.json()
+    .then(data => {
+      setFood(data.food)
+      console.log(data.food)
+      
+    })
+  })
+}
+
+useEffect(() => {
+  foods();
+}, []);
+
+console.log("this is outside" ,food)
 
   return (
    <div>
 
    <h1> TracMac </h1>
+
+   <div>
+  <ul>
+    { food.map(food =>  ( 
+      
+      <li key={food.id} > {food.name} </li>
+    ))}
+    </ul> 
+
+
+   </div>
+  
    <Header totalCal={totalCal} />
    <CalForm />
   
